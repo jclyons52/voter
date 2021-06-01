1ac9c8f5c455fe720c013feec3358c9b8b07e03d
Joseph Lyons <jclyons52@gmail.com>
2021-06-01 21:00:25 +1000

    wip

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -4,9 +4,6 @@ module.exports = {
 	devServer: {
 		disableHostCheck: true
 	},
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/voter/'
-    : '/',
 	configureWebpack: {
 		resolve: {
 			symlinks: false,
